a074a581cee64550c6d504e21ef3f8fd4b6c0f81
Abhay-Blacklisted <blacklisted.web16@gmail.com>
2026-07-09 18:12:51 +0530

    Test fix

--- src/components/layouts/Footer/footer.data.ts
+++ src/components/layouts/Footer/footer.data.ts
@@ -15,7 +15,7 @@ export const quickLinksCol1: NavLink[] = [
 ];
 
 export const quickLinksCol2: NavLink[] = [
-  { label: "Distributor Connect", href: "https://directorque.com/login",    external: true, },
+  { label: "Distributor Connect", href: "https://directorque.com/login", external: true },
   { label: "Depot", href: "/depot" },
   { label: "Torque Online Stores", href: "https://www.torqueonline.co.in/" },
   { label: "Torque HRMS", href: "https://torque.keka.com/" },
