0120afcd6370428fe253669ab4534171adcfcea8
Abhay-Blacklisted <blacklisted.web16@gmail.com>
2026-07-14 16:23:36 +0530

    Fix Related Events section types fix

--- src/components/sections/events/RelatedEventsSection/RelatedEventsSection.types.ts
+++ src/components/sections/events/RelatedEventsSection/RelatedEventsSection.types.ts
@@ -3,5 +3,5 @@ import type { Event } from "@/types/event";
 export interface RelatedEventsSectionProps {
   events: Event[];
   className?: string;
-  tag: string | null | undefined;
+  tag?: string | null | undefined;
 }
